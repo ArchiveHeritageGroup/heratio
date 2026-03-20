16ed263188d3cf31d5a3c6740b9b328f1c33413b
Your Name <you@example.com>
2026-03-20 10:55:39 +0200

    v0.30.38: Match AtoM sort button text: 'Sort by: ...' prefix on all browse pages

--- packages/ahg-core/resources/views/components/sort-pickers.blade.php
+++ packages/ahg-core/resources/views/components/sort-pickers.blade.php
@@ -1,7 +1,6 @@
 <div class="dropdown">
-  <button class="btn btn-sm atom-btn-white dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <i class="fas fa-sort" aria-hidden="true"></i>
-    {{ $options[request('sort', $default ?? 'lastUpdated')] ?? 'Sort' }}
+  <button class="btn btn-sm atom-btn-white dropdown-toggle text-wrap" type="button" id="sort-button" data-bs-toggle="dropdown" aria-expanded="false">
+    Sort by: {{ $options[request('sort', $default ?? 'lastUpdated')] ?? 'Sort' }}
   </button>
   <ul class="dropdown-menu dropdown-menu-end">
     @foreach($options as $key => $label)
