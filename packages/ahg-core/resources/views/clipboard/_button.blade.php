7865592da0249ddb28ed5b1793f13002e46d1c19
Your Name <you@example.com>
2026-05-03 17:20:03 +0200

    v1.47.17: Clipboard button: wrap hardcoded 'Add' / 'Remove' / 'Add to clipboard' / 'Remove from clipboard' strings in __() so the lang/{locale}.json overrides actually apply. Verified: af   culture now renders data-title='Voeg by' / data-alt-title='Verwyder' (was 'Add' / 'Remove') on the IO show page.

--- packages/ahg-core/resources/views/clipboard/_button.blade.php
+++ packages/ahg-core/resources/views/clipboard/_button.blade.php
@@ -17,11 +17,11 @@
   data-clipboard-slug="{{ $slug }}"
   data-clipboard-type="{{ $type }}"
   @unless($wide) data-tooltip="true" @endunless
-  data-title="{{ $wide ? 'Add' : 'Add to clipboard' }}"
-  data-alt-title="{{ $wide ? 'Remove' : 'Remove from clipboard' }}"
-  title="{{ $wide ? 'Add to clipboard' : 'Add to clipboard' }}">
+  data-title="{{ $wide ? __('Add') : __('Add to clipboard') }}"
+  data-alt-title="{{ $wide ? __('Remove') : __('Remove from clipboard') }}"
+  title="{{ __('Add to clipboard') }}">
   <i class="fas fa-lg fa-paperclip" aria-hidden="true"></i>
   <span class="{{ $wide ? 'ms-2' : 'visually-hidden' }}">
-    {{ $wide ? 'Add to clipboard' : 'Add to clipboard' }}
+    {{ __('Add to clipboard') }}
   </span>
 </button>
