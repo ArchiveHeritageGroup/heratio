4d489d78bc4fb479060b702e357a38b4910d82f5
Your Name <you@example.com>
2026-05-25 18:11:00 +0200

    test(ahg-share-link): add PHPUnit scaffold and example test (audit #678)

---
+++ packages/ahg-share-link/tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace AhgShareLink\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testBasic()
+    {
+        $this->assertTrue(true);
+    }
+}
