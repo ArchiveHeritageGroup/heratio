ebc34e68725233cdf39aa987e64a8a69bfea92f6
Johan Pieterse <johan@theahg.co.za>
2026-08-08 10:30:09 +0200

    v1.154.529: chore(core): remove 20 dead/duplicate Blade views from ahg-core. These were pre-Bootstrap-5-theme originals left orphaned after the theme rewrite: 4 partials superseded by their live theme:: twins (_privacy-message, _print-preview-bar, _google-analytics, _tag-manager) plus 16 zero-reference orphans (_breadcrumb, _bs2deprecation-message, _contact-information, edit-physical-objects, error401/404/500, _ga-institutions-dimension, _generic-picker, _print-preview-button, _sidebar-pager, _sortable-table-header, term-permission, translate-permission, _update-check, _view-picker). Verified unused repo-wide: zero ahg-core:: references, zero bare/dynamic view() names, zero route/JS refs; full view:cache compiles clean after removal. No code paths reference them.

--- packages/ahg-core/resources/views/_ga-institutions-dimension.blade.php
+++
@@ -1,3 +0,0 @@
-@php slot('google_analytics'); @endphp
-  ga('set', 'dimension@php echo $dimensionIndex; @endphp', '@php echo $repository->getAuthorizedFormOfName(['sourceCulture' => true]); @endphp');
-@php end_slot(); @endphp
--- packages/ahg-core/resources/views/_print-preview-button.blade.php
+++
@@ -1,6 +0,0 @@
-<a
-  class="btn btn-sm atom-btn-white"
-  href="{{ request()->fullUrlWithQuery(['media' => 'print']) }}">
-  <i class="fas fa-print me-1" aria-hidden="true"></i>
-  {{ __('Print preview') }}
-</a>
